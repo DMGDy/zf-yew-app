fc1a566106bd84fcf3fccfbf694a628c627153fe
DMGDy <dylangarza1909@gmail.com>
2024-10-10 14:20:33 -0400

    states: removed offline error state and renamed idle to Offline

--- src/states.rs
+++ src/states.rs
@@ -2,44 +2,41 @@ use serde::Deserialize;
 
 #[derive(Deserialize,Clone)]
 pub enum State {
-    Awake,
+    Offline,
+    Online,
     InProgress,
     Done,
-    Idle,
     ENoFirmware,
     ENoRead,
     ENoWrite,
     EOpen,
     EUnknown,
-    EOffline
 }
 
 impl State {
     pub fn from_i32(n: i32) -> State {
         match n {
-            0 => Self::Idle,
-            1 => Self::Awake,
+            0 => Self::Offline,
+            1 => Self::Online,
             2 => Self::InProgress,
             3 => Self::Done,
             -1 => Self::ENoFirmware,
             -2 => Self::ENoRead,
             -3 => Self::ENoWrite,
             -4 => Self::EOpen,
-            -5 => Self::EOffline,
             _ => Self:: EUnknown,
         }
     }
     pub fn code(&self) -> i32 {
         match self {
-            Self::Idle => 0,
-            Self::Awake => 1,
+            Self::Offline => 0,
+            Self::Online => 1,
             Self::InProgress => 2,
             Self::Done => 3,
             Self::ENoFirmware=> -1,
             Self::ENoRead=> -2,
             Self::ENoWrite=> -3,
             Self::EOpen=> -4,
-            Self::EOffline=> -5,
             Self::EUnknown=> -6,
 
         }
@@ -47,15 +44,14 @@ impl State {
 
     pub fn message(&self) -> &str{
         match self {
-            Self::Idle => "Waiting for server to load...",
-            Self::Awake => "Server is up. Waiting for test to begin.",
+            Self::Offline => "Trying to connect to server...",
+            Self::Online => "Server is up. Waiting for test to begin.",
             Self::InProgress => "Microcontroller has began testing",
             Self::Done => "Test results are available",
             Self::ENoFirmware => "No Firmware was found for the selected device",
             Self::ENoRead => "There was an error reading data from the microcontroller",
             Self::ENoWrite => "There was an error writing data to the microcontroller",
             Self::EOpen => "There was an error trying to communicate to the microcontroller",
-            Self::EOffline => "The server is offline, trying to connect again...",
             Self::EUnknown => "Something bad went wrong, check browser console",
         }
     }
